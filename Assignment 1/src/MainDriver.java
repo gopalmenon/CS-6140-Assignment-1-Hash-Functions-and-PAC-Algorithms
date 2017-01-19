113d89e4801e2f70ba2306b05e345f9a75ac7f36
Gopal Menon <gopal.mnn@gmail.com>
2017-01-18 23:55:01 -0700

    Added code for Octave plotting of run time for birthday paradox.

--- Assignment 1/src/MainDriver.java
+++ Assignment 1/src/MainDriver.java
@@ -27,8 +27,8 @@ public class MainDriver {
 		Map<Integer, Double> plotValues = BirthdayParadox.getCumulativeDensityPlot(BirthdayParadox.NUMBER_OF_ITERATIONS, BirthdayParadox.DEFAULT_DOMAIN_SIZE);
 		System.out.println("B: Cumulative Density Plot:\n" + getCumulativeDensityPlotRScript(plotValues));
 		System.out.println("C: Expected Trials till Collision: " + this.decimalFormat.format(getExpectedTrialsToCollision(plotValues)));
-		Map<TrialsAndDomainSize, Long> runTimes = BirthdayParadox.getRunTimes();
-		System.out.println("D: Run Times: " + runTimes);
+		System.out.println("D: Run Times Script:\n" + getOctaveRunTimesPlotScript());
+
 	}
 
 	/**
@@ -74,6 +74,50 @@ public class MainDriver {
 			totalNumberOfTrials += trial.intValue();
 		}
 		return (double) totalNumberOfTrials / numberOfTrails.size();
+	}
+	
+	private String getOctaveRunTimesPlotScript() {
+		
+		StringBuffer octaveRunTimesPlotScript = new StringBuffer();
+		
+		Map<TrialsAndDomainSize, Long> runTimes = BirthdayParadox.getRunTimes();
+		
+		octaveRunTimesPlotScript.append("tx = [").append(BirthdayParadox.NUMBER_OF_TRIALS_SETTINGS.get(0).intValue());
+		for (int trialsIndex = 1; trialsIndex < BirthdayParadox.NUMBER_OF_TRIALS_SETTINGS.size(); ++trialsIndex) {
+			octaveRunTimesPlotScript.append(", ").append(BirthdayParadox.NUMBER_OF_TRIALS_SETTINGS.get(trialsIndex).intValue());
+		}
+		octaveRunTimesPlotScript.append("];\n");
+			
+		octaveRunTimesPlotScript.append("ty = [").append(BirthdayParadox.DOMAIN_SIZE_SETTINGS.get(0).intValue());
+		for (int domainSizeIndex = 1; domainSizeIndex < BirthdayParadox.DOMAIN_SIZE_SETTINGS.size(); ++domainSizeIndex) {
+			octaveRunTimesPlotScript.append(", ").append(BirthdayParadox.DOMAIN_SIZE_SETTINGS.get(domainSizeIndex).intValue());
+		}
+		octaveRunTimesPlotScript.append("];\n");
+	
+		octaveRunTimesPlotScript.append("[xx, yy] = meshgrid (tx, ty);\n");
+		
+		octaveRunTimesPlotScript.append("tz = [");
+		boolean firstTime = true;
+		for (int domainSizeIndex = 0; domainSizeIndex < BirthdayParadox.DOMAIN_SIZE_SETTINGS.size(); ++domainSizeIndex) {
+			firstTime = true;
+			for (int trialsIndex = 0; trialsIndex < BirthdayParadox.NUMBER_OF_TRIALS_SETTINGS.size(); ++trialsIndex) {
+				if (firstTime) {
+					firstTime = false;
+				} else {
+					octaveRunTimesPlotScript.append(", ");
+				}
+				octaveRunTimesPlotScript.append(runTimes.get(new TrialsAndDomainSize(BirthdayParadox.NUMBER_OF_TRIALS_SETTINGS.get(trialsIndex), BirthdayParadox.DOMAIN_SIZE_SETTINGS.get(domainSizeIndex))).longValue());
+			}
+			if (domainSizeIndex != BirthdayParadox.DOMAIN_SIZE_SETTINGS.size() - 1) {
+				octaveRunTimesPlotScript.append("; ");
+			}
+		}
+		octaveRunTimesPlotScript.append("]\n");
+		
+		octaveRunTimesPlotScript.append("mesh (tx, ty, tz);");
+		
+		return octaveRunTimesPlotScript.toString();
+	
 	}
 	
 }
